cb1f231f363dacdb16fd70f4e74db663f78ae4f6
Tosin Akinosho <takinosh@redhat.com>
2021-09-29 11:44:47 -0400

    updating env variables

--- aro4-build.sh
+++ aro4-build.sh
@@ -57,20 +57,52 @@ then
     export BUILDDATE
 fi 
 
-CLUSTER="aro-$(whoami)-$RAND"
-export CLUSTER
-RESOURCEGROUP="$CLUSTER-$LOCATION"
-export RESOURCEGROUP
-SUBID="$(az account show -o tsv --query id)"
-export SUBID
-VNET_NAME="$CLUSTER-vnet"
-export VNET_NAME
-VNET_OCTET1="$(echo $VNET | cut -f1 -d.)"
-export VNET_OCTET1
-VNET_OCTET2="$(echo $VNET | cut -f2 -d.)"
-export VNET_OCTET2
+if [ -z ${CLUSTER} ];
+then 
+    CLUSTER="aro-$(whoami)-$RAND"
+    echo "##vso[task.setvariable variable=CLUSTER]$CLUSTER"
+    export CLUSTER
+fi 
+
+if [ -z ${RESOURCEGROUP} ];
+then 
+    RESOURCEGROUP="$CLUSTER-$LOCATION"
+    echo "##vso[task.setvariable variable=RESOURCEGROUP]$RESOURCEGROUP"
+    export RESOURCEGROUP
+fi 
+
+if [ -z ${SUBID} ];
+then 
+    SUBID="$(az account show -o tsv --query id)"
+    echo "##vso[task.setvariable variable=SUBID]$SUBID"
+    export SUBID
+fi 
+
+if [ -z ${VNET_NAME} ];
+then 
+    VNET_NAME="$VNET_NAME-vnet"
+    echo "##vso[task.setvariable variable=VNET_NAME]$VNET_NAME"
+    export VNET_NAME
+fi 
+
+
+if [ -z ${VNET_OCTET1} ];
+then 
+    VNET_OCTET1="$(echo $VNET | cut -f1 -d.)"
+    echo "##vso[task.setvariable variable=VNET_OCTET1]$VNET_OCTET1"
+    export VNET_OCTET1
+fi 
+
+if [ -z ${VNET_OCTET2} ];
+then 
+    VNET_OCTET2="$(echo $VNET | cut -f2 -d.)"
+    echo "##vso[task.setvariable variable=VNET_OCTET2]$VNET_OCTET2"
+    export VNET_OCTET2
+fi 
+
 if [ -z "$VNET_RG" ]; then
     VNET_RG="$RESOURCEGROUP"
+    echo "##vso[task.setvariable variable=VNET_RG]$VNET_RG"
     export VNET_RG
 fi
 
