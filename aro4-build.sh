199c794e0f0d88b185dd7ed47a1252c9e74f9cb6
Tosin Akinosho <takinosh@redhat.com>
2021-09-29 15:18:51 -0400

    test

--- aro4-build.sh
+++ aro4-build.sh
@@ -229,7 +229,8 @@ function configure_networking(){
     echo "done"
     echo -n "Adding ARO RP Contributor access to VNET..."
     #az role assignment create --scope /subscriptions/$SUBID/resourceGroups/$RESOURCEGROUP/providers/Microsoft.Network/virtualNetworks/$VNET_NAME --assignee ${ROLE_ASSIGNEE}  --role "Contributor" -o table > /dev/null
-    sleep 15s
+    echo "az role assignment create --scope /subscriptions/$SUBID/resourceGroups/$RESOURCEGROUP/providers/Microsoft.Network/virtualNetworks/$VNET_NAME  --assignee-object-id ${ROLE_ASSIGNEE}  --role "Contributor" --assignee-principal-type ServicePrincipal"
+    sleep 60s
     az role assignment create --scope /subscriptions/$SUBID/resourceGroups/$RESOURCEGROUP/providers/Microsoft.Network/virtualNetworks/$VNET_NAME  --assignee-object-id ${ROLE_ASSIGNEE}  --role "Contributor" --assignee-principal-type ServicePrincipal > /dev/null
     echo "done"
     exit 0
