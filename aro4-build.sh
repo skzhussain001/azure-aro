e8becd0ca8ed37cf8465c87836367602e3c151dc
Tosin Akinosho <takinosh@redhat.com>
2021-09-30 18:14:49 -0400

    testing code

--- aro4-build.sh
+++ aro4-build.sh
@@ -257,7 +257,7 @@ function configure_networking(){
     az network vnet subnet update -g "$RESOURCEGROUP" --vnet-name "$VNET_NAME" -n "$CLUSTER-master" --disable-private-link-service-network-policies true -o table > /dev/null
     echo "done"
     #echo -n "Adding ARO RP Contributor access to VNET..."
-    az role assignment create --scope /subscriptions/$SUBID/resourceGroups/$RESOURCEGROUP/providers/Microsoft.Network/virtualNetworks/$VNET_NAME --assignee ${ROLE_ASSIGNEE}  --role "Contributor" -o table > /dev/null
+    #az role assignment create --scope /subscriptions/$SUBID/resourceGroups/$RESOURCEGROUP/providers/Microsoft.Network/virtualNetworks/$VNET_NAME --assignee ${ROLE_ASSIGNEE}  --role "Contributor" -o table > /dev/null
     ######
     ### REMOVING FOR NOW
     ######
