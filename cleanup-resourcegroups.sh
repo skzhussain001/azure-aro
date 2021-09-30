c9a5e29bdb4efc6f4b77ae6b83c589c8e20c1ac0
Tosin Akinosho <takinosh@redhat.com>
2021-09-30 18:07:33 -0400

    testing code

--- cleanup-resourcegroups.sh
+++ cleanup-resourcegroups.sh
@@ -4,6 +4,7 @@
 ARO_GROUPS=$(az group list | jq .[].name | grep aro)
 for aro_grp in ${ARO_GROUPS}
 do
+    aro_grp=$(echo $aro_grp | sed 's/ *$//g' | sed "s/['\"]//g")
     echo "DELETE ${aro_grp}"
     az group delete  --name ${aro_grp} --yes
 done
