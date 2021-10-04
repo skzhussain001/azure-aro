f6314251d2edf7e3a397d955974e64ec5e2d0775
Tosin Akinosho <takinosh@redhat.com>
2021-10-04 16:27:20 -0400

    another test

--- aro4-build.sh
+++ aro4-build.sh
@@ -336,7 +336,7 @@ function create_aro_cluster(){
         echo "done."
     fi
 
-    time az aro create -g "$RESOURCEGROUP" -n "$CLUSTER" --cluster-resource-group "$RESOURCEGROUP-cluster" --vnet="$VNET_NAME" --vnet-resource-group="$VNET_RG" --master-subnet="$CLUSTER-master" --worker-subnet="$CLUSTER-worker" --ingress-visibility="$INGRESSPRIVACY" --apiserver-visibility="$APIPRIVACY" --worker-count="$WORKERS" --master-vm-size="$MASTER_SIZE" --worker-vm-size="$WORKER_SIZE" $CUSTOMDOMAIN --pull-secret=$PULLSECRET  --client-id $servicePrincipalId  --client-secret $servicePrincipalKey --debug -o table
+    time az aro create -g "$RESOURCEGROUP" -n "$CLUSTER" --cluster-resource-group "$RESOURCEGROUP-cluster" --vnet="$VNET_NAME" --vnet-resource-group="$VNET_RG" --master-subnet="$CLUSTER-master" --worker-subnet="$CLUSTER-worker" --ingress-visibility="$INGRESSPRIVACY" --apiserver-visibility="$APIPRIVACY" --worker-count="$WORKERS" --master-vm-size="$MASTER_SIZE" --worker-vm-size="$WORKER_SIZE" $CUSTOMDOMAIN --pull-secret=$PULLSECRET  --client-id $servicePrincipalId  --client-secret $servicePrincipalKey  -o table
 
 
     ################################################################################################## Post Provisioning
